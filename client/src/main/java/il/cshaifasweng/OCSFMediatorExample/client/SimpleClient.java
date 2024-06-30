05182328e00432ea23554992462883739945dbe8
Nim_Abr <4br4hams@gmail.com>
2024-06-30 16:18:35 +0300

    Temporary commit

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
@@ -2,11 +2,21 @@ package il.cshaifasweng.OCSFMediatorExample.client;
 
 import il.cshaifasweng.OCSFMediatorExample.client.ocsf.AbstractClient;
 import il.cshaifasweng.OCSFMediatorExample.entities.Message;
+import il.cshaifasweng.OCSFMediatorExample.entities.MessageObject;
+import il.cshaifasweng.OCSFMediatorExample.client.PrimaryController;
+import il.cshaifasweng.OCSFMediatorExample.entities.Movie;
 import org.greenrobot.eventbus.EventBus;
 
 import java.io.IOException;
+import java.util.List;
 
 public class SimpleClient extends AbstractClient {
+
+    private static final String EMPTY_MESSAGE_REQUEST = "blank";
+    private static final String SHOW_ALL_MOVIES_REQUEST = "show all movies";
+    private static final String CHANGE_SCREENING_TIMES_REQUEST = "change screening times of the movie";
+    private static final String UPDATE_MOVIES_LIST_REQUEST = "update movies list";
+
 
     private static SimpleClient client = null;
     private static String ipAddress = "localhost";
@@ -41,11 +51,13 @@ public class SimpleClient extends AbstractClient {
 
     @Override
     protected void handleMessageFromServer(Object msg) {
-        Message message = (Message) msg;
-        String messageString = message.getMessage();
-        if (messageString.equals("update submitters IDs")) {
-            EventBus.getDefault().post(new UpdateMessageEvent(message));
-        } else if (messageString.equals("client added successfully")) {
+        MessageObject message = (MessageObject) msg;
+        String messageString = message.getMsg();
+//        if (messageString.equals("show all movies")) {
+//            messageHandler.updateMovies((List<Movie>)message.getObject()); //List<Movie> movies
+//        } else
+//
+        if (messageString.equals("client added successfully")) {
             EventBus.getDefault().post(new NewSubscriberEvent(message));
         } else if (messageString.equals("Error! we got an empty message")) {
             EventBus.getDefault().post(new ErrorEvent(message));
