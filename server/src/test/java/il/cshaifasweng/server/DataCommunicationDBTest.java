5974dae59f98428254f32e654926bbdc33a476ae
evya1 <103887518+evya1@users.noreply.github.com>
2024-07-13 18:22:12 +0300

    feat: Update testing and build configurations
    - Updated GitHub Actions workflow to include test results from all modules by changing the path to '**/target/surefire-reports/'.
    - Added new JUnit 5 test cases for Theater entity to test various functionalities.
    - Updated DataCommunicationDBTest to use JUnit 5 assertions.
    - Enhanced test cases with logging for better debugging and information tracking.
    - Updated POM files to use JUnit 5 and improve dependency management.

--- server/src/test/java/il/cshaifasweng/server/DataCommunicationDBTest.java
+++ server/src/test/java/il/cshaifasweng/server/DataCommunicationDBTest.java
@@ -1,20 +1,18 @@
 package il.cshaifasweng.server;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class DataCommunicationDBTest
-{
+public class DataCommunicationDBTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
